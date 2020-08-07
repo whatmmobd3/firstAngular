b82af1caf42a376cbeae50fed664f85c39f2f1f4
LocNguyen <quangloc.nguyen@mafc.com.vn>
2020-08-07 19:40:20 +0700

    component styles

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-servers',
   templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.css']
+  styleUrls: ['./servers.component.css'],
 })
 export class ServersComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
