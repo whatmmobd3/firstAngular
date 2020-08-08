02180940fb6c4c5605f4f9886bcff8cd24000800
LocNguyen <quangloc.nguyen@mafc.com.vn>
2020-08-08 16:47:09 +0700

    solution

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,9 +10,17 @@ import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
 import { SuccessAlertComponent } from './success-alert/success-alert.component';
 import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import { TestComponent } from './test/test.component';
 
 @NgModule({
-  declarations: [AppComponent, ServerComponent, ServersComponent, SuccessAlertComponent, WarningAlertComponent],
+  declarations: [
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    SuccessAlertComponent,
+    WarningAlertComponent,
+    TestComponent,
+  ],
   imports: [BrowserModule, FormsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
