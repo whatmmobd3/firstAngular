f94098c5d3b508b31a24ad1190034fc39e9615c5
LocNguyen <quangloc.nguyen@mafc.com.vn>
2020-08-08 16:23:12 +0700

    exercise two way binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -6,7 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css'],
 })
 export class ServersComponent implements OnInit {
+  userName: String = '';
+  isNull:boolean = true;
   constructor() {}
 
   ngOnInit(): void {}
+
+  changeName(e:Event) {
+    this.userName =(<HTMLInputElement>e.target).value
+    if (this.userName !== '') {
+      this.isNull = false
+    }
+
+  }
+
+  resetNull(){
+    this.userName = '';
+    this.isNull = true;
+  }
 }
